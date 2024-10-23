575d19df499ca7a8e8253908b6f88ca608cefbb6
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2024-10-23 16:28:00 +0300

    update23102024

---
+++ software/PersonelApp/PersonelApp.Web/Controllers/DerslerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelApp.Web.Controllers
+{
+    public class DerslerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ software/PersonelApp/PersonelApp.Web/Controllers/İlanlarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelApp.Web.Controllers
+{
+    public class İlanlarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
